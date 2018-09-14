3d317aa3f88d28d18a29339359f09d0e749dfe06
ahscuml <ahscuml@163.com>
2018-09-14 22:27:18 +0800

    增加22,216,526

---
+++ src/Q357CountNumberswithUniqueDigits.java
@@ -0,0 +1,15 @@
+/**
+ * @author ahscuml
+ * @date 2018/9/14
+ * @time 22:08
+ */
+public class Q357CountNumberswithUniqueDigits {
+    public static void main(String[] args) {
+
+    }
+
+    private int count = 0;
+    public int countNumbersWithUniqueDigits(int n) {
+        int totalsum = (int)Math.pow(10,n);
+    }
+}
