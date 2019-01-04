e126c1e378cdb3e6af393d9b1d8bd3b227fedc8c
ahscuml <ahscuml@163.com>
2019-01-04 10:55:00 +0800

    108

---
+++ src/Q108ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,49 @@
+/**
+ * @author ahscuml
+ * @date 2019/1/4
+ * @time 10:49
+ */
+public class Q108ConvertSortedArraytoBinarySearchTree {
+    /**
+     * 测试函数
+     * */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 利用递归的方法来说实现
+     * */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return null;
+        }
+        // 节点个数可能是奇数或者偶数，需要进行判断
+        int n = nums.length;
+        return helper(nums, 0, n - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left ,int right) {
+        if(right < left) {
+            return null;
+        }
+        int mid = (right + left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+
+    /**
+     * 树结构的定义
+     * */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
