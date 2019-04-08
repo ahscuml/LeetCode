403e464f2a3582f2052892d5cbcbe8769e86e773
ahscuml <ahscuml@163.com>
2019-04-09 00:45:57 +0800

    补充了两道做过的DP问题

--- src/Q279PerfectSquares.java
+++ src/Q279PerfectSquares.java
@@ -18,14 +18,14 @@ public class Q279PerfectSquares {
      * 动态规划方法
      * */
     public static int numSquares(int n) {
-        int count[] = new int[n + 1];
-        Arrays.fill(count, Integer.MAX_VALUE);
-        count[0] = 0;
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j * j <=i; j++) {
-                count[i] = Math.min(count[i],count[i - j * j] + 1);
+                dp[i] = Math.min(dp[i],dp[i - j * j] + 1);
             }
         }
-        return count[n];
+        return dp[n];
     }
 }
